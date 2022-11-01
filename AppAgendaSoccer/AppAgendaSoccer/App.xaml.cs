68b4bc9e8fcde5771d4ba21c4d5ca32850e6a494
rafael altieris <raphaelaltieris@gmail.com>
2022-11-01 20:03:36 -0300

    fist commit

---
+++ AppAgendaSoccer/AppAgendaSoccer/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppAgendaSoccer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new View.Login());
+
+            /*if (Properties.ContainsKey("usuario_logado"))
+                MainPage = new NavigationPage(new View.Home());
+            else
+                MainPage = new NavigationPage(new View.Login());*/
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppAgendaSoccer/AppAgendaSoccer/View/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppAgendaSoccer.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        App PropriedadesApp;
+
+        public Login()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            PropriedadesApp = (App)Application.Current;
+        }
+
+        private void Button_Clicked_Entrar(object sender, EventArgs e)
+        {
+            string usuario = txt_email.Text;
+            string senha = txt_senha.Text;
+
+            string usuario_correto = "rafael";
+            string senha_correta = "1234";
+
+            if (usuario == usuario_correto && senha == senha_correta)
+            {
+                /*App.Current.Properties.Add("usuario_logado", usuario);
+                App.Current.MainPage = new Home();*/
+
+                Navigation.PushAsync(new Home());
+
+            }
+            else
+                DisplayAlert("Ops!", "Usuário ou senha incorretos.", "OK");
+        }
+
+        private void Button_Clicked_Criar(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
